b7f1a6c2c0b7a0ce211f88086ef46e4ac496c676
Alex Grigorovich <alex.grigorovich@gmail.com>
2011-01-30 03:42:25 +0600

    Clean up sequence_tests

--- src/sequence_tests.erl
+++ src/sequence_tests.erl
@@ -2,26 +2,24 @@
 -include_lib("eunit/include/eunit.hrl").
 
 new_seq_should_be_empty_test() ->
-    ?assertEqual(0, sequence:length(sequence:new())),
-    ?assertEqual([], sequence:to_list(sequence:new())).
+    S = sequence:new(),
+    ?assertEqual(0, sequence:length(S)),
+    ?assertEqual([], sequence:to_list(S)).
 
 insert_of_element_into_empty_seq_should_increment_length_test() ->
-    Seq = sequence:insert(1, sequence:new()),
-    ?assertEqual(1, sequence:length(Seq)),
-    ?assertEqual([1], sequence:to_list(Seq)).
+    S = seq_from_list([1]),
+    ?assertEqual(1, sequence:length(S)),
+    ?assertEqual([1], sequence:to_list(S)).
 
 insert_of_larger_element_should_increase_sequence_test() ->
-    Seq = sequence:new(),
-    Seq1 = sequence:insert(1, Seq),
-    Seq2 = sequence:insert(2, Seq1),
-    Seq3 = sequence:insert(3, Seq2),
-    ?assertEqual(3, sequence:length(Seq3)),
-    ?assertEqual([1,2,3], sequence:to_list(Seq3)).
+    S = seq_from_list([1,2,3]),
+    ?assertEqual(3, sequence:length(S)),
+    ?assertEqual([1,2,3], sequence:to_list(S)).
 
-insert_of_smaller_element_should_reset_sequence() ->
-    Seq = sequence:new(),
-    Seq1 = sequence:insert(2, Seq),
-    Seq2 = sequence:insert(1, Seq1),
-    Seq3 = sequence:insert(2, Seq2),
-    ?assertEqual(2, sequence:length(Seq3)),
-    ?assertEqual([1,2], sequence:to_list(Seq3)).
+insert_of_smaller_element_should_reset_sequence_test() ->
+    S = seq_from_list([2,1,2]),
+    ?assertEqual(2, sequence:length(S)),
+    ?assertEqual([1,2], sequence:to_list(S)).
+
+seq_from_list(L) ->
+    lists:foldl(fun sequence:insert/2, sequence:new(), L).
