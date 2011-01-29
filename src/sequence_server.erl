8e25aa2789133e25b333a65498db360f3f34e97c
Alex Grigorovich <alex.grigorovich@gmail.com>
2011-01-29 20:14:54 +0600

    Switch sequence_server to be a gen_server

--- src/sequence_server.erl
+++ src/sequence_server.erl
@@ -1,38 +1,50 @@
 -module(sequence_server).
 -compile(export_all).
+-behaviour(gen_server).
 
+% gen_server exports
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+% API
 start() ->
-    SS = spawn(?MODULE, loop, [[]]),
-    register(sequence_server, SS).
+    gen_server:start({local, ?MODULE}, ?MODULE, [], []).
 
-loop(L) ->
-    receive
-        {Pid, stop} ->
-            Pid ! {self(), ok};
-        {Pid, {register, Num}} ->
-            Pid ! {self(), ok},
-            L1 = update_sequence(Num, L),
-            loop(L1);
-        {Pid, get} ->
-            Pid ! {self(), lists:reverse(L)},
-            loop(L)
+-spec(stop() -> ok).
+stop() ->
+    gen_server:call(?MODULE, stop).
+
+-spec(register(integer()) -> ok).
+register(Val) ->
+    gen_server:call(?MODULE, {register, Val}).
+
+-spec(get() -> [integer()]).
+get() ->
+    gen_server:call(?MODULE, get).
+
+% gen_server callbacks
+init(_Arg) ->
+    {ok, []}.
+
+handle_call(Request, _From, Seq) ->
+    case Request of
+        {register, Num} ->
+            {reply, ok, update_sequence(Num, Seq)};
+        get ->
+            {reply, lists:reverse(Seq), Seq};
+        stop ->
+            {stop, normal, ok, Seq}
     end.
 
 update_sequence(Num, []) -> [Num];
 update_sequence(Num, L = [H|_T]) when Num > H -> [Num | L];
 update_sequence(Num, [H|_T]) when Num =< H -> [Num].
 
-stop() ->
-    rpc(whereis(sequence_server), stop).
+handle_cast(_Request, Seq) ->
+    {noreply, Seq}.
 
-register(Val) ->
-    rpc(whereis(sequence_server), {register, Val}).
+handle_info(_Info, Seq) ->
+    {noreply, Seq}.
 
-get() ->
-    rpc(whereis(sequence_server), get).
+terminate(_Reason, _Seq) -> ok.
 
-rpc(Pid, Q) ->
-    Pid ! { self(), Q },
-    receive
-        { Pid, Val } -> Val
-    end.
+code_change(_OldVsn, Seq, _Extra) -> {ok, Seq}.
