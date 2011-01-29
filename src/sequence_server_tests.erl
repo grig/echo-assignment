9b000ec6bbe3de1508d65b1857a36468f4be666a
Alex Grigorovich <alex.grigorovich@gmail.com>
2011-01-29 20:16:14 +0600

    Clean up tests

--- src/sequence_server_tests.erl
+++ src/sequence_server_tests.erl
@@ -5,7 +5,7 @@ put_test_() ->
     {foreach,
      fun setup/0,
      fun cleanup/1,
-     [{"should register value", fun should_register_value/0},
+     [fun should_register_value/0,
       fun should_return_empty_list_on_empty_sequence/0,
       fun should_return_single_element/0,
       fun should_return_sequential_elements/0,
@@ -22,7 +22,7 @@ should_register_value() ->
     ok = sequence_server:register(1).
 
 should_return_empty_list_on_empty_sequence() ->
-    ?assert(sequence_server:get() =:= []).
+    ?assertEqual([], sequence_server:get()).
 
 should_return_single_element() ->
     sequence_server:register(1),
