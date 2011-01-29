08cb8755b0723f49d7134a7ac9dc7f357b1a7d94
Alex Grigorovich <alex.grigorovich@gmail.com>
2011-01-30 03:49:48 +0600

    Clean up sequence_tests
    sequence_tests.erl: replace duplicate assertion code with a macro
    "?assertSeqEqual"

--- src/sequence_tests.erl
+++ src/sequence_tests.erl
@@ -1,25 +1,25 @@
 -module(sequence_tests).
 -include_lib("eunit/include/eunit.hrl").
 
+-define(assertSeqEqual(List, Seq),
+        ?assertEqual(length(List), sequence:length(Seq)),
+        ?assertEqual(List, sequence:to_list(Seq))).
+
 new_seq_should_be_empty_test() ->
-    S = sequence:new(),
-    ?assertEqual(0, sequence:length(S)),
-    ?assertEqual([], sequence:to_list(S)).
+    S = seq_from_list([]),
+    ?assertSeqEqual([], S).
 
 insert_of_element_into_empty_seq_should_increment_length_test() ->
     S = seq_from_list([1]),
-    ?assertEqual(1, sequence:length(S)),
-    ?assertEqual([1], sequence:to_list(S)).
+    ?assertSeqEqual([1], S).
 
 insert_of_larger_element_should_increase_sequence_test() ->
     S = seq_from_list([1,2,3]),
-    ?assertEqual(3, sequence:length(S)),
-    ?assertEqual([1,2,3], sequence:to_list(S)).
+    ?assertSeqEqual([1,2,3], S).
 
 insert_of_smaller_element_should_reset_sequence_test() ->
     S = seq_from_list([2,1,2]),
-    ?assertEqual(2, sequence:length(S)),
-    ?assertEqual([1,2], sequence:to_list(S)).
+    ?assertSeqEqual([1,2], S).
 
 seq_from_list(L) ->
     lists:foldl(fun sequence:insert/2, sequence:new(), L).
