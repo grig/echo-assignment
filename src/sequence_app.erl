8d21f7d0a8c7baa97aaf0dce79eb04cf1ab5794e
Alex Grigorovich <alex.grigorovich@gmail.com>
2011-02-03 21:34:26 +0600

    Clean up sequence_app start routines

--- src/sequence_app.erl
+++ src/sequence_app.erl
@@ -1,24 +1,21 @@
 -module(sequence_app).
--compile(export_all).
+-export([start/0, start/1, stop/0]).
 
 start() ->
-    {ok, Cwd} = file:get_cwd(),
-    Port = 8081,
-    yaws:start_embedded(Cwd,
-                        [{port, Port},
-                         {listen, {0, 0, 0, 0}},
-                         {appmods, [{"/", sg_appmod}]}]),
-    sequence_server:start_link().
+    start(8081, 1).
 
 start(ConfigFile) ->
-    {ok, Cwd} = file:get_cwd(),
     Config = seq_conf:load(ConfigFile),
     Port = seq_conf:get_env(Config, port, 8081),
+    MaxSequences = seq_conf:get_env(Config, max_sequences, 1),
+    start(Port, MaxSequences).
+
+start(Port, MaxSequences) ->
+    {ok, Cwd} = file:get_cwd(),
     yaws:start_embedded(Cwd,
                         [{port, Port},
                          {listen, {0, 0, 0, 0}},
                          {appmods, [{"/", sg_appmod}]}]),
-    MaxSequences = seq_conf:get_env(Config, max_sequences, 1),
     sequence_server:start_link([{max_sequences, MaxSequences}]).
 
 stop() ->
