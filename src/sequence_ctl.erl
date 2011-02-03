32a4b715595d4c2e5793e8e90e9ec4f8cdc1660e
Alex Grigorovich <alex.grigorovich@gmail.com>
2011-02-04 04:08:12 +0600

    Clean up sequence_ctl.erl

--- src/sequence_ctl.erl
+++ src/sequence_ctl.erl
@@ -1,35 +1,29 @@
 -module(sequence_ctl).
-% controlling functions to be used during accptance tests
+% Stupid remote-control interface for sequence server
 
 -compile(export_all).
 
 start_app() ->
-  {ok, Hostname} = inet:gethostname(),
-  Node = list_to_atom("cucumber@"++Hostname),
-  spawn(Node, fun() -> sequence_app:start() end),
+  ctl(fun() -> sequence_app:start() end),
   init:stop().
 
 stop_app() ->
-  {ok, Hostname} = inet:gethostname(),
-  Node = list_to_atom("cucumber@"++Hostname),
-  spawn(Node, fun() -> init:stop() end),
+  ctl(fun() -> init:stop() end),
   init:stop().
 
 start_server() ->
-  {ok, Hostname} = inet:gethostname(),
-  Node = list_to_atom("cucumber@"++Hostname),
-  spawn(Node, fun() -> sequence_server:set_conf([{max_sequences, 1}]) end),
+  ctl(fun() -> sequence_server:set_conf([{max_sequences, 1}]) end),
   init:stop().
 
 stop_server() ->
-  {ok, Hostname} = inet:gethostname(),
-  Node = list_to_atom("cucumber@"++Hostname),
-  %% spawn(Node, fun() -> sequence_server:stop() end),
   init:stop().
 
 reload() ->
-  {ok, Hostname} = inet:gethostname(),
-  Node = list_to_atom("cucumber@"++Hostname),
   Config = seq_conf:load("tmp/sequence_server.config"),
-  spawn(Node, fun() -> sequence_server:set_conf(Config) end),
+  ctl(fun() -> sequence_server:set_conf(Config) end),
   init:stop().
+
+ctl(F) ->
+    {ok, Hostname} = inet:gethostname(),
+    Node = list_to_atom("cucumber@"++Hostname),
+    spawn(Node, F).
