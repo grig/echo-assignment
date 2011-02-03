c18396aca5df19071ea7c4f233bd04227781c467
Alex Grigorovich <alex.grigorovich@gmail.com>
2011-02-03 19:52:55 +0600

    Extract server control module from support scripts
    sequence_ctl.erl: add functions for controller sequence server
    application

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,9 +1,9 @@
 #system("/Users/grig/local/yaws/bin/yaws --daemon")
 system("erl -pa ebin -pa /Users/grig/local/yaws/lib/yaws/ebin -sname cucumber -set-cookie cucumber -noshell&")
 puts "starting sequence server..."
-system("erl -pa ebin -sname cucumber_ctl -set-cookie cucumber -noshell -eval 'spawn_link(cucumber@host2, fun() -> sequence_app:start() end), init:stop().'")
+system("erl -pa ebin -sname cucumber_ctl -set-cookie cucumber -noshell -s sequence_ctl start_app")
 puts "done"
 
 at_exit do
-  system("erl -pa ebin -sname cucumber_ctl -set-cookie cucumber -noshell -eval 'spawn_link(cucumber@host2, fun() -> init:stop() end), init:stop().'")
+  system("erl -pa ebin -sname cucumber_ctl -set-cookie cucumber -noshell -s sequence_ctl stop_app")
 end
---
+++ src/sequence_ctl.erl
@@ -0,0 +1,34 @@
+-module(sequence_ctl).
+% controlling functions to be used during accptance tests
+
+-compile(export_all).
+
+start_app() ->
+  {ok, Hostname} = inet:gethostname(),
+  Node = list_to_atom("cucumber@"++Hostname),
+  spawn_link(Node, fun() -> sequence_app:start() end),
+  init:stop().
+
+stop_app() ->
+  {ok, Hostname} = inet:gethostname(),
+  Node = list_to_atom("cucumber@"++Hostname),
+  spawn_link(Node, fun() -> init:stop() end),
+  init:stop().
+
+start_server() ->
+  {ok, Hostname} = inet:gethostname(),
+  Node = list_to_atom("cucumber@"++Hostname),
+  spawn_link(Node, fun() -> sequence_server:start_link() end),
+  init:stop().
+
+stop_server() ->
+  {ok, Hostname} = inet:gethostname(),
+  Node = list_to_atom("cucumber@"++Hostname),
+  spawn_link(Node, fun() -> sequence_server:stop() end),
+  init:stop().
+
+reload() ->
+  {ok, Hostname} = inet:gethostname(),
+  Node = list_to_atom("cucumber@"++Hostname),
+  Config = seq_conf:load("tmp/sequence_server.config"),
+  spawn_link(Node, fun() -> sequence_server:set_conf(Config) end), init:stop().
